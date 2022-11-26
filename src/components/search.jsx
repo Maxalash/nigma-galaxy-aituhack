add599d45a33c4abbb8b09392fdbe478c2f05100
Makhmut <mahmudlyder@gmail.com>
2022-11-26 15:52:16 +0600

    feat: add load data from the server

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,15 +1,20 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './home.css';
 import './search.css';
 import { useDispatch, useSelector } from "react-redux";
 import { setCount } from '../reducers/reposReducer';
 import { productdata, filters } from '../assets/data';
+import { url, admin } from '../assets/fetchdata';
+
+
 
 const Search = () => {
+  const [allproducts, setAllProducts] = useState(productdata);
   const [isActive, setActive] = useState(false);
-  const [didReach, setReach] = useState(true);
-  const [filteredProducts, setProducts] = useState(productdata);
+  const [didReach, setReach] = useState(false);
+  const [filteredProducts, setProducts] = useState(allproducts);
   const [filterCategories, setCategories] = useState([]);
+  const [getFilter, setFilter] = useState([])
   const filterComponent = useRef()
   const failMessage = useRef()
   const dispatch = useDispatch()
@@ -37,38 +42,38 @@ const Search = () => {
     switch (filterType) {
       case "Все разделы": items = productdata;
         break;
-      case "Телефоны и гаджеты": items = productdata.filter((it) => it.categories[0] === "Телефоны и гаджеты");
+      case "Телефоны и гаджеты": setFilter(filters["Телефоны и гаджеты"]);
         break;
-      case "Компьютеры": items = productdata.filter((it) => it.categories[0] === "Компьютеры");
+      case "Компьютеры": setFilter(filters["Компьютеры"]);
         break;
-      case "Фото": items = productdata.filter((it) => it.categories[0] === "Фото");
+      case "Фото": setFilter(filters["Фото"]);
         break;
-      case "Телевизоры": items = productdata.filter((it) => it.categories[0] === "Телевизоры");
+      case "Телевизоры": setFilter(filters["Телевизоры"]);
         break;
-      case "Аудио": items = productdata.filter((it) => it.categories[0] === "Аудио");
+      case "Аудио": setFilter(filters["Аудио"]);
         break;
-      case "Бытовая техника": items = productdata.filter((it) => it.categories[0] === "Бытовая техника");
+      case "Бытовая техника": setFilter(filters["Бытовая техника"]);
         break;
-      case "Климат": items = productdata.filter((it) => it.categories[0] === "Климат");
+      case "Климат": setFilter(filters["Климат"]);
         break;
-      case "Дом": items = productdata.filter((it) => it.categories[0] === "Дом");
+      case "Дом": setFilter(filters["Дом"]);
         break;
-      case "Детские товары": items = productdata.filter((it) => it.categories[0] === "Детские товары");
+      case "Детские товары": setFilter(filters["Детские товары"]);
         break;
-      case "Авто": items = productdata.filter((it) => it.categories[0] === "Авто");
+      case "Авто": setFilter(filters["Авто"]);
         break;
-      case "Инструмент": items = productdata.filter((it) => it.categories[0] === "Инструмент");
+      case "Инструмент": setFilter(filters["Инструмент"]);
         break;
-      case "Туризм": items = productdata.filter((it) => it.categories[0] === "Туризм");
+      case "Туризм": setFilter(filters["Туризм"]);
         break;
-      case "Спорт": items = productdata.filter((it) => it.categories[0] === "Спорт");
+      case "Спорт": setFilter(filters["Спорт"]);
         break;
-      case "Часы и украшения": items = productdata.filter((it) => it.categories[0] === "Часы и украшения");
+      case "Часы и украшения": setFilter(filters["Часы и украшения"]);
         break;
       default: items = items;
     }
 
-    if (items.length == 0) {
+    if (items.length < 0) {
       failMessage.current.style.display = "block";
     } else {
       failMessage.current.style.display = "none";
@@ -78,16 +83,85 @@ const Search = () => {
     setCategories(filters[filterType])
   }
 
+  // Function for filtering products according to set filters in the filter form
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const pricefrom = e.target.children[1].children[2].value;
+    const priceto = e.target.children[1].children[4].value;
+    console.log(typeof priceto)
+    let items = JSON.parse(JSON.stringify(filteredProducts));
+    if (priceto == "") {
+      items = items.filter((it) => it.price >= pricefrom);
+    } else if (pricefrom == "") {
+      items = items.filter((it) => it.price <= priceto);
+    } else {
+      items = items.filter((it) => it.price >= pricefrom && it.price <= priceto);
+    }
+    setProducts(items)
+  }
+
+  const handleRadio = (e)=>{
+    // e.preventDefault()
+    const filte = e.target.id
+    if(!filte==""){
+      setFilter([filte])
+    }
+    
+  }
+
   // Filter toggler handler function
   const toggle = (e) => {
     setActive(!isActive);
   }
 
+  // Get data when filter changed
+  useEffect(() => {
+    console.log(getFilter)
+    async function fetchData(){
+      await fetch(`${url}/api/get_items/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${admin}`
+        },
+        body: JSON.stringify({
+          item: getFilter[0]
+        })
+      }).then(data=>{
+        return data.json();
+      }).then(dat=>{
+        console.log(dat);
+        setProducts(dat)
+      }).catch(err=>{
+        console.log(err);
+      });
+    }
+
+    fetchData()
+    
+
+  }, [getFilter])
+
+  const pop = () => {
+    if (window.scrollY > 90) {
+      setReach(true);
+    }else{
+      setReach(false);
+    }
+
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', pop);
+  
+    return () => window.removeEventListener('scroll', pop);
+  },[]);
+
   return (
     <div className='filter-section'>
       <div className={`filter-bar ${didReach ? "is-fixed" : ""}`} ref={filterComponent} onScroll={(e) => handleScroll(e)}>
-        <div className='filter-burger' onClick={(e) => toggle(e)}></div>
-        <div className='filter-button' onClick={(e) => toggle(e)}>Filter</div>
+        <div className='filter-button' onClick={(e) => toggle(e)}><img src='filter.png' width="30%" /></div>
+        {/* <div className='filter-button' onClick={(e) => toggle(e)}>Filter</div> */}
         <ul className='filter-types'>
           {Object.keys(filters)?.map((item, key) => {
             return (
@@ -97,28 +171,28 @@ const Search = () => {
         </ul>
       </div>
       <div className={`filter ${isActive ? "is-active" : ""} ${didReach ? "is-fixed" : ""}`}>
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <div className="filter-block">
             <h4>Категорий</h4>
-            <ul className="filter-content">
+            <div className="filter-content" >
               {filterCategories?.map((item, key) => {
                 return (
-                  <li key={key}>
-                    <input className="filter-check" type="checkbox" id={`checkbox${key}`} />
-                    <label className="checkbox-label" htmlFor={`checkbox${key}`}>{item}</label>
-                  </li>
+                  <label className="form-control" id={item} key={key} onClick={(e)=>handleRadio(e)}>
+                    <input className="filter-check" type="radio" name="radio"/>
+                    {item}
+                  </label>
                 )
               })}
-            </ul>
+            </div>
           </div>
           <div className="filter-block">
             <h4>Цена</h4>
-            <label className="checkbox-label" htmlFor="pricefrom">От</label>
-            <input className="filter-textbox" type="number" id="pricefrom" />
-            <br/>
-            <label className="checkbox-label" htmlFor="priceto">До</label>
+            <label className="textbox-label" htmlFor="pricefrom">От</label>
+            <input className="filter-textbox" type="number" id="pricefrom" /><br/>
+            <label className="textbox-label" htmlFor="priceto">До</label>
             <input className="filter-textbox" type="number" id="priceto" />
           </div>
+          <input className='filter-submit' type="submit" value="Подобрать" />
         </form>
       </div>
       <div className={`filter-kanban ${isActive ? "is-shrinked" : ""}`}>
@@ -126,15 +200,15 @@ const Search = () => {
           {Array.isArray(filteredProducts) ? filteredProducts?.map((item, key) => {
             return (
               <li className="card" key={key}>
-                <img src="#" alt={item.name} /><br />
-                Name: {item.name}<br />
-                Categories: {item.categories}<br />
-                Price: {item.price}$
+                <img src={item.item_image} alt={item.item_name} />
+                <div className='card-title'>{item.item_name}</div>
+                {/* Description: {item.item_description}<br /> */}
+                {/* Price: {item.price}$ */}
               </li>
             )
           }) : ""}
         </ul>
-        <div ref={failMessage} className="kanban-fail-message">No results found</div>
+        <div ref={failMessage} className="kanban-fail-message">Не найдено ни одного товара</div>
       </div>
     </div>
   );
