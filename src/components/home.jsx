5846afcb78997c066fee32c8be15a75898850320
Makhmut <mahmudlyder@gmail.com>
2022-11-26 15:51:27 +0600

    refactor: remove footer

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './home.css';
 import { useDispatch, useSelector } from "react-redux";
 import { setCount } from '../reducers/reposReducer';
 import Header from '../components/header.jsx';
 import Footer from '../components/footer.jsx';
 import Search from '../components/search.jsx';
+import Login from './login.jsx';
+import { productdata } from '../assets/data';
+import { addProduct } from '../reducers/productsReducer';
 
 const Home = () => {
   const dispatch = useDispatch()
-  const count = useSelector(state => state.repos.count)
+  const prodData = useSelector(state => state.products)
 
-  function onCountClick() {
-    dispatch(setCount(6))
-  }
+  // Load data when component did mount
+  useEffect(() => {
+    // if(prodData.length== 0){
+    //   dispatch(addProduct(productdata));
+    // }
+  });
+
   return (
     <div className='app'>
+      <Login />
       <Header />
       <Search />
-      <Footer />
     </div>
   );
 }
